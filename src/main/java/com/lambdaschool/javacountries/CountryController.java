2c49fef6d4ce7671039a7a03b4792a3dd3d6b8b7
Kenya-a <Kenya-a@users.noreply.github.com>
2019-09-09 18:39:53 -0500

    .

--- src/main/java/com/lambdaschool/javacountries/CountryController.java
+++ src/main/java/com/lambdaschool/javacountries/CountryController.java
@@ -26,7 +26,7 @@ public class CountryController
 
     //localhost:2019/data/start/k
         //return the countries alphabetically that begin with the given letter
-    @GetMapping(value = "start/{letter}",
+    @GetMapping(value = "/start/{letter}",
                 produces = {"application/json"})
     public ResponseEntity<?> getCountryByFirstLetter(@PathVariable char letter)
     {
@@ -38,11 +38,32 @@ public class CountryController
     //localhost:2019/data/size/25
         //return the countries alphabetically that have a name equal to or longer than the given length
 
+//    @GetMapping(value = "/size/{num}",
+//                produces = {"application/json"})
+//    public ResponseEntity<?> getCountryBySize()
+//    {
+//        ArrayList<Country> rtnCountry = JavaCountriesApplication.myCountryList.countryList;
+//        rtnCountry.sort((c1,c2) -> c1.getName() - c2.getName());
+//
+//    }
+
     //localhost:2019/data/people
         //return the countries that have a population equal to or greater than the given population
 
     //localhost:2019/data/min
         //return the country with the smallest population
+
+//    @GetMapping(value = "/size/{num}", produces = {"application/json"})
+//        public ResponseEntity<?> getCountryByMinPopulationSize(@PathVariable char num)
+//        {
+//            ArrayList<Country> rtnCount1 = JavaCountriesApplication.myCountryList
+//                    .findCountries(c -> c.getName().toUpperCase().charAt(0) == Character.toUpperCase(letter));
+//
+//            rtnCount1.sort((c1,c2) -> ((int)c1.getPopulation() - c2.getPopulation()));
+//
+//            return new ResponseEntity<>(rtnCount1, HttpStatus.OK);
+//
+//        }
 
     //localhost:2019/data/max
         //return the country with the largest population
